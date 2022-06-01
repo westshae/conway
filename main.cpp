20b3b0b8f80b962c87cd577e2658ebf694918b93
westshae <shaewest02@gmail.com>
2022-06-01 15:31:11 +1200

    Init

---
+++ main.cpp
@@ -0,0 +1,10 @@
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  string test;
+  cout << "Input please::" << endl;
+  cin >> test;
+  cout << "Input recieved is:: " << test;
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+Compile
+g++ main.cpp
