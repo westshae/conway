2f078a32d7b8d6d3fbf75ae1ac6843a830a48962
westshae <shaewest02@gmail.com>
2022-06-01 15:51:45 +1200

    Created cell class

---
+++ c++/conway.cpp
@@ -0,0 +1,16 @@
+
+#include "conway.hpp"
+using namespace std;
+
+namespace conway{
+  class Conway:public AbstractConway {
+    public:
+      //TODO Contructor (takes size of board)
+      //TODO generateBoard()
+      //TODO getTile()
+      //TODO setTile()
+      //TODO nearbyNeighbour()
+    private:
+      //TODO var 2d array
+  };
+}
--- c++/conway.hpp
+++ c++/conway.hpp
@@ -1,15 +1,61 @@
+#ifndef __ABSTRACT_DB_HPP__
+#define __ABSTRACT_DB_HPP__
 
+#include<tuple>
 
+using namespace std;
 namespace conway{
+  class Cell{
+    public:
+      //Constructor
+      Cell(int x_in, int y_in, bool alive_in){
+        x = x_in;
+        y = y_in;
+        alive = alive_in;
+      }
+
+      //Returns coords as a pair.
+      pair<int,int> getCoords(){
+        return make_pair(x,y);
+      }
+
+      //getter for 'alive' bool
+      bool isAlive(){
+        return alive;
+      }
+
+      //setter for 'alive' bool
+      void setAlive(bool alive_in){
+        alive = alive_in;
+      }
+
+      //Returns # if cell alive, else ' '
+      char toChar(){
+        if(alive){
+          return '#';
+        }else{
+          return ' ';
+        }
+      }
+
+
+    private:
+      int x;
+      int y;
+      bool alive;
+  };
   class AbstractConway{
     public:
-
+      //TODO generateBoard()
+      virtual bool generateBoard(int board_size);
+      //TODO getCell()
+      //TODO setCell()
+      //TODO nearbyNeighbour()
     private:
   };
+  
 
-  class Conway{
-    public:
-
-    private:
-  };
+  
 }
+
+#endif /* __ABSTRACT_DB_HPP__ */
