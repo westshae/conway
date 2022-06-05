966ffc5feef9d9e0eeddc29acd91f5c1e48555a4
westshae <shaewest02@gmail.com>
2022-06-05 17:54:00 +1200

    parse_input_to_int function

--- c++/main.cpp
+++ c++/main.cpp
@@ -3,15 +3,29 @@
 #include"conway.cpp"
 using namespace std;
 
+int parse_input_to_int(string print_first);
+
 int main(){
-  Conway *conway = new Conway(10);
+  
+  int board_size = parse_input_to_int("What would you like the board size to be?: \n");
+
+  Conway *conway = new Conway(board_size);
+
+  // while(true){
+  //   int result = parse_input_to_int("Input:: ");
+  // }
   conway->setCell(0,0,true);
   conway->setCell(2,2,true);
   conway->setCell(0,1, true);
   conway->setCell(2,1, true);
   conway->printBoard();
-  // string test;
-  // cout << "Input please::" << endl;
-  // cin >> test;
-  // cout << "Input recieved is:: " << test;
+}
+
+int parse_input_to_int(string print_first){
+  string input;
+  int value;
+  cout<< print_first;
+  cin >> input;
+  value = stoi(input);
+  return value;
 }
