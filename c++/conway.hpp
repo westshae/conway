99c7e9bc627e2068a2ad761be10b4d4f1e5e1d30
westshae <shaewest02@gmail.com>
2022-06-05 21:22:58 +1200

    Added initial shapes, plus warning message connected to border rules

--- c++/conway.hpp
+++ c++/conway.hpp
@@ -1,69 +1,81 @@
-#include<tuple>
+#include <tuple>
 
 using namespace std;
-namespace conway{
-  class Cell{
-    public:
-      //Constructor
-      Cell(int x_in, int y_in, bool alive_in){
-        x = x_in;
-        y = y_in;
-        alive = alive_in;
+namespace conway
+{
+  class Cell
+  {
+  public:
+    // Constructor
+    Cell(int x_in, int y_in, bool alive_in)
+    {
+      x = x_in;
+      y = y_in;
+      alive = alive_in;
+      change_alive = false;
+    }
+
+    // Returns coords as a pair.
+    pair<int, int> get_coords()
+    {
+      return make_pair(x, y);
+    }
+
+    // getter for 'alive' bool
+    bool is_alive()
+    {
+      return alive;
+    }
+
+    // Tells the cell to update when called.
+    void toggle_alive()
+    {
+      change_alive = true;
+    }
+
+    // Updates alive after all cells have been changed.
+    void update_alive()
+    {
+      if (change_alive)
+      {
+        alive = !alive;
         change_alive = false;
       }
+    }
 
-      //Returns coords as a pair.
-      pair<int,int> get_coords(){
-        return make_pair(x,y);
+    // Returns # if cell alive, else ' '
+    char to_char()
+    {
+      if (alive)
+      {
+        return '#';
       }
-
-      //getter for 'alive' bool
-      bool is_alive(){
-        return alive;
+      else
+      {
+        return ' ';
       }
+    }
 
-      //setter for 'alive' bool
-      void toggle_alive(){
-        change_alive = true;
-      }
-
-      //Updates alive after all cells have been changed.
-      void update_alive(){
-        if(change_alive){
-          alive = !alive;
-          change_alive = false;
-        }
-      }
-
-      //Returns # if cell alive, else ' '
-      char to_char(){
-        if(alive){
-          return '#';
-        }else{
-          return '+';
-        }
-      }
-
-
-
-    private:
-      int x;
-      int y;
-      bool alive;
-      bool change_alive;
+  private:
+    int x;
+    int y;
+    bool alive;
+    bool change_alive;
   };
-  class AbstractConway{
-    public:
-      virtual void reset_board(int board_size) = 0;
-      virtual void reset_board(int x_size, int y_size) = 0;
-      virtual int board_height() = 0;
-      virtual int board_width() = 0;
-      virtual Cell* get_cell(int x, int y) = 0;
-      virtual void set_cell(int x, int y, bool alive) = 0;
-      virtual int alive_neighbours(int x, int y) = 0;
-      virtual void print_board() = 0;
-    private:
-      virtual bool valid_cell(int x, int y) = 0;
+  class AbstractConway
+  {
+  public:
+    virtual void reset_board(int board_size) = 0;
+    virtual void reset_board(int x_size, int y_size) = 0;
+    virtual int board_height() = 0;
+    virtual int board_width() = 0;
+    virtual Cell *get_cell(int x, int y) = 0;
+    virtual void set_cell(int x, int y, bool alive) = 0;
+    virtual int alive_neighbours(int x, int y) = 0;
+    virtual void print_board() = 0;
+
+  private:
+    virtual bool valid_cell(int x, int y) = 0;
   };
 
 }
--- c++/main.cpp
+++ c++/main.cpp
@@ -15,6 +15,7 @@ struct command
 
 int main()
 {
+  cout << "Please note, because conway doesn't have any border rules, this program does have errors along those borders due to infinity being impossible." << endl;
   string input;
   int board_size = 1;
   cout << "Enter board size:: " << endl;
@@ -23,9 +24,16 @@ int main()
 
   Conway *conway = new Conway(board_size);
 
-  conway->set_cell(5,5, true);
-    conway->set_cell(5,6,true);
-  conway->set_cell(5,7,true);
+  conway->set_cell(7, 5, true);
+  conway->set_cell(7, 6, true);
+  conway->set_cell(7, 7, true);
+
+  conway->set_cell(4, 2, true);
+  conway->set_cell(4, 3, true);
+  conway->set_cell(4, 4, true);
+  conway->set_cell(3, 4, true);
+  conway->set_cell(2, 3, true);
+
 
   while (true)
   {
@@ -39,20 +47,24 @@ int main()
         Cell *cell = conway->get_cell(x, y);
         int neigh = conway->alive_neighbours(x, y);
 
-        if(cell->is_alive()){
-          if(conway->alive_neighbours(x,y) < 2){
+        if (cell->is_alive())
+        {
+          if (conway->alive_neighbours(x, y) < 2)
+          {
             cell->toggle_alive();
           }
-          else if(conway->alive_neighbours(x,y) > 3){
+          else if (conway->alive_neighbours(x, y) > 3)
+          {
             cell->toggle_alive();
           }
         }
-        else{
-          if(conway->alive_neighbours(x,y) == 3){
+        else
+        {
+          if (conway->alive_neighbours(x, y) == 3)
+          {
             cell->toggle_alive();
           }
         }
-        
       }
     }
 
